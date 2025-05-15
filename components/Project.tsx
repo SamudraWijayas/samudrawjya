e8d2bd24a88d688fd10e962dbb52ff17d1866aea
SamudraWijayas <swijayasamdoria@gmail.com>
2025-05-15 17:11:49 +0700

    update

--- components/Project.tsx
+++ components/Project.tsx
@@ -87,11 +87,11 @@ const Project = () => {
         </span>
       </h1>
 
-      <div className="flex flex-wrap items-center justify-center p-4 gap-16 mt-10">
+      <div className="flex flex-wrap items-center justify-center p-4 gap-16 mt-10 overflow-x-hidden">
         {projects.map((projek) => (
           <div
             key={projek.id}
-            className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]"
+            className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-full max-w-sm"
           >
             <PinContainer title={projek.title} href={projek.link}>
               <div className="relative flex items-center justify-center sm:w-96 w-[80vw] h-[20vh] lg:h-[30vh] mb-10 rounded-3xl overflow-hidden bg-[#13162d]">
